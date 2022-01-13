bf78fb7fd139f0385d510543d8ea8b71d2b5f54c
alex <alex.km@live.com>
2022-01-13 21:13:20 +0300

    added unit tests

---
+++ Example01Tests/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Exercise01;
+
+namespace Example01Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOnes()
+        {
+            //arrange
+            long num = 1;
+            //act
+            var stringValue = num.Towards();
+            var expectedValue = "One";
+            //assert
+            Assert.AreEqual(expectedValue, stringValue);
+        }
+
+        [TestMethod]
+        public void TestTens()
+        {
+            //arrange
+            long num = 16;
+            //act
+            var stringValue = num.Towards();
+            var expectedValue = "Sixteen";
+            //assert
+            Assert.AreEqual(expectedValue, stringValue);
+        }
+
+ 
+
+        [TestMethod]
+        public void TestThousands()
+        {
+            //arrange
+            long num = 1567;
+
+            //act
+            var stringValue = num.Towards();
+            var expectedValue = "One Thousand Five Hundred Sixty Seven";
+            //assert
+            Assert.AreEqual(expectedValue, stringValue);
+        }
+
+        [TestMethod]
+        public void TestMillions()
+        {
+            //arrange
+            long num = 18000000;
+
+            //act
+            var stringValue = num.Towards();
+            var expectedValue = "Eighteen Million";
+            //assert
+            Assert.AreEqual(expectedValue, stringValue);
+        }
+
+        [TestMethod]
+        public void TestQuintillion()
+        {
+            //arrange
+            long num = 18456002032011000007;
+
+            //act
+            var stringValue = num.Towards();
+            var expectedValue = "Eighteen Quintillion, Four Hundred Fifty Six Quadrillion Two Trillion, Thirty Two Billion Eleven Million Seven";
+            //assert
+            Assert.AreEqual(expectedValue, stringValue);
+        }
+    }
+}
